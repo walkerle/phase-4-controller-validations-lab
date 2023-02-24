a81563ff763da0f10ad3c0260d4b47eff8e05ff5
Walker Le <walker.le88@gmail.com>
2023-02-23 17:21:37 -0700

    commit tree cc97e4c9cfc5af2fc78ad6c6e73c68a9dbad9a73

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,10 @@ class PostsController < ApplicationController
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
-    render json: post
+    post.update!(post_params)
+    render json: post, status: :ok
+  rescue ActiveRecord::RecordInvalid => invalid
+      render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   private
